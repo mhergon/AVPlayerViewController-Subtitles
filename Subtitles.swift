3d7a62c4c03529d2fb102b717605402d687b26b9
Maksym Bilenko <sath891@gmail.com>
2016-11-14 19:11:38 +0200

    Fix regex due to couple timestamp standards

--- Subtitles.swift
+++ Subtitles.swift
@@ -137,7 +137,7 @@ public extension AVPlayerViewController {
                 let index = (group as NSString).substring(with: i.range)
                 
                 // Get "from" & "to" time
-                regex = try NSRegularExpression(pattern: "\\d{1,2}:\\d{1,2}:\\d{1,2},\\d{1,3}", options: .caseInsensitive)
+                regex = try NSRegularExpression(pattern: "\\d{1,2}:\\d{1,2}:\\d{1,2}[.,]\\d{1,3}", options: .caseInsensitive)
                 match = regex.matches(in: group, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, group.characters.count))
                 guard match.count == 2 else {
                     continue
