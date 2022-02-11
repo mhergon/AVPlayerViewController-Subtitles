36c88712b5f69ca8ff820cd2a226a0092e6ff12e
Crt Gregoric <crt.gregoric@gmail.com>
2022-02-11 21:49:37 +0100

    Example code cleanup

--- Example/AVPlayerViewController-Subtitles/ViewController.swift
+++ Example/AVPlayerViewController-Subtitles/ViewController.swift
@@ -7,25 +7,13 @@
 //
 
 import UIKit
-import MediaPlayer
 import AVKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     //MARK:- Actions
+    
     @IBAction func showVideo(_ sender: UIButton) {
-        
         // Video file
         let videoFile = Bundle.main.path(forResource: "trailer_720p", ofType: "mov")
                 
@@ -53,11 +41,9 @@ class ViewController: UIViewController {
         
         // Play
         moviePlayer.player?.play()
-        
     }
     
     func subtitleParser() {
-        
         // Subtitle file
         let subtitleFile = Bundle.main.path(forResource: "trailer_720p", ofType: "srt")
         let subtitleURL = URL(fileURLWithPath: subtitleFile!)
@@ -67,8 +53,6 @@ class ViewController: UIViewController {
         
         // Do something with result
         _ = parser.searchSubtitles(at: 2.0) // Search subtitle at 2.0 seconds
-        
     }
     
 }
-
